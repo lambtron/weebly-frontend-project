31221dd98620e74e34b7c62e23a649638b1b4cf9
Andy Jiang <andyjiang@gmail.com>
2014-02-18 21:19:07 -0800

    saving changes

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -4,5 +4,65 @@ weeblyApp.controller('mainController',
   ['$scope', '$http',
 	function ($scope, $http)
 {
+  // Global state variable.
+  var state = $scope.state = {
+    pages: [],
+    elements: [],
+    siteGridOn: false,
+    newPageTitle: '',
+    selectedPage: {},
+    addPage: function addPage () {
+      if (this.newPageTitle === '')
+        this.newPageTitle = 'Page';
+      var newPage = new Page(this.newPageTitle);
+      this.pages.push(newPage);
+      this.newPageTitle = '';
+      this.selectedPage = newPage;
+    },
+    deletePage: function deletePage (index) {
+      this.pages.splice(index, 1);
+    },
+    selectPage: function selectPage (page) {
+      this.selectedPage = page;
+    },
+    toggleSiteGrid: function toggleSiteGrid () {
+      this.siteGridOn = !this.siteGridOn;
+    }
+  };
 
+  // Page object.
+  function Page (title) {
+    this.title = title;
+    this.elements = [];
+    this.editTitle = function editTitle (title) {
+      this.title = title;
+    };
+    this.addElement = function addElement (type) {
+      var newElement = new Element(type);
+      this.elements.push(newElement);
+    };
+    this.deleteElement = function deleteElement (id) {
+
+    };
+  }
+
+  // Element object.
+  function Element (type, location, size) {
+    this.type = type;
+    this.location = location; // location = {x: 0, y: 0}
+    this.size = size;         // size = {x: 0, y: 0}
+  }
+
+  function initializePage () {
+    var titleElement = new Element('title')
+      , textElement = new Element('text')
+      , imageElement = new Element('image')
+      , navElement = new Element('nav');
+    state.elements.push(titleElement, textElement, imageElement, navElement);
+  }
+
+  initializePage();
+  state.addPage();
+  state.pages[0].addElement('image')
+  state.addPage();
 }]);
--- public/js/directives.js
+++ public/js/directives.js
@@ -1,3 +1,34 @@
 'use strict';
 
 // Directives.
+weeblyApp.directive('ngDraggable', function() {
+  return {
+    restrict: 'A',
+    link: function(scope, elem, attrs, ctrl) {
+      var options = scope.$eval(attrs.ngDraggable);
+
+      elem.draggable();
+      // On release, add it to state.selectedPage.elements
+      // - make sure to include location, size, type
+      var replacement = elem;
+      var parent = $(elem).parent();
+      elem.on('dragstop', function (event, ui) {
+        // When drag stops, create a new div in its place.
+        // replacement.remove();
+        // parent.html($(replacement));
+
+        // Add elem to state.selectedPage.elements
+        var element = scope.element;
+        element.location = {
+          x: event.clientX,
+          y: event.clientY
+        };
+        element.size = {
+          x: 100,
+          y: 100
+        };
+        options.page.elements.push(element);
+      });
+    }
+  };
+});
